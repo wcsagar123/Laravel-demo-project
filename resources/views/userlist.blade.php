140493289189f4857e91a330bb42b86eaaf74e9c
webcontrive <info@webcontrive.com>
2020-09-11 14:54:34 +0530

    ne file changes

--- resources/views/userlist.blade.php
+++ resources/views/userlist.blade.php
@@ -20,7 +20,7 @@
         @if( $user->image != '' )
         <td><img src='{{ asset("uploads/$user->image") }}' ></td>
         @else
-        <td>No Image tes test test gdfjkghduhygudfngbjdfsghui</td>
+        <td>No Image Gdfhdgfhdgfhds cgjikfigjdfi kmfksdjtgdsjtoig</td>
         @endif 
         <td>{{$user->name}}</td>
         <td>{{$user->email}}</td>
